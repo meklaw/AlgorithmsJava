6528bcc1586040e4529166fbaa226aadc565b65c
Никита Ульянов <nik.ule@yandex.ru>
2022-09-07 11:00:07 +0300

    init BST

---
+++ src/test/java/BSTTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BSTTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findNodeByKey() {
+    }
+
+    @Test
+    void addKeyValue() {
+    }
+
+    @Test
+    void finMinMax() {
+    }
+
+    @Test
+    void deleteNodeByKey() {
+    }
+
+    @Test
+    void count() {
+    }
+}
