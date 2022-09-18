583e5c4778b8f269a13b82cbffc072e0b323d7f8
Никита Ульянов <nik.ule@yandex.ru>
2022-09-18 21:18:11 +0300

    add: GenerateBBSTArray method

---
+++ src/main/java/AlgorithmsDataStructures2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class AlgorithmsDataStructures2 {
+    public static int[] GenerateBBSTArray(int[] a) {
+        Arrays.sort(a);
+        int[] resultBST = generateBinaryArray(a.length);
+        arrayToBST(resultBST, 0, a, 0, a.length - 1);
+        return resultBST;
+    }
+
+    protected static void arrayToBST(int[] aBST, int element, int[] fromArr, int start, int end) {
+        int mid = (start + end) / 2;
+        aBST[element] = fromArr[mid];
+        if (start == end)
+            return;
+
+
+        int leftChild = element * 2 + 1;
+        if (leftChild < aBST.length && mid != (start + mid - 1) / 2 && fromArr[(start + mid - 1) / 2] != aBST[0])
+            arrayToBST(aBST, leftChild, fromArr, start, mid - 1);
+
+        int rightChild = element * 2 + 2;
+        if (rightChild < aBST.length && mid != (mid + 1 + end) / 2)
+            arrayToBST(aBST, rightChild, fromArr, mid + 1, end);
+
+    }
+
+    protected static int[] generateBinaryArray(int length) {
+        int newLength = 1;
+        int pow = 0;
+        while (length >= newLength) {
+            newLength = (int) Math.pow(2, pow);
+            pow++;
+        }
+
+        return new int[newLength - 1];
+    }
+}
---
+++ src/test/java/AlgorithmsDataStructures2Test.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AlgorithmsDataStructures2Test {
+
+    protected int[] abs1 = {50, 25, 75, 20, 37, 62, 84};
+    protected int[] abs2 = {50, 25, 75, 20, 37, 84};
+    protected int[] abs3 = {50, 25, 75, 20, 84};
+    protected int[] abs4 = {50};
+    protected int[] abs5 = {50, 75};
+
+    @Test
+    void generateBBSTArray() {
+        int[] generateBBSTArray1 = AlgorithmsDataStructures2.GenerateBBSTArray(abs1);
+        int[] generateBBSTArray2 = AlgorithmsDataStructures2.GenerateBBSTArray(abs2);
+        int[] generateBBSTArray3 = AlgorithmsDataStructures2.GenerateBBSTArray(abs3);
+        int[] generateBBSTArray4 = AlgorithmsDataStructures2.GenerateBBSTArray(abs4);
+        int[] generateBBSTArray5 = AlgorithmsDataStructures2.GenerateBBSTArray(abs5);
+
+        assertEquals(generateBBSTArray1[0], 50);
+        assertEquals(generateBBSTArray1[1], 25);
+        assertEquals(generateBBSTArray1[2], 75);
+        assertEquals(generateBBSTArray1[3], 20);
+        assertEquals(generateBBSTArray1[4], 37);
+        assertEquals(generateBBSTArray1[5], 62);
+        assertEquals(generateBBSTArray1[6], 84);
+
+        System.out.println(Arrays.toString(generateBBSTArray3));
+        assertEquals(generateBBSTArray3[0], 50);
+        assertEquals(generateBBSTArray3[1], 20);
+        assertEquals(generateBBSTArray3[2], 75);
+        assertEquals(generateBBSTArray3[3], 0);
+        assertEquals(generateBBSTArray3[4], 25);
+        assertEquals(generateBBSTArray3[5], 0);
+        assertEquals(generateBBSTArray3[6], 84);
+
+        assertEquals(generateBBSTArray4[0], 50);
+
+        assertEquals(generateBBSTArray5[0], 50);
+        assertEquals(generateBBSTArray5[1], 0);
+        assertEquals(generateBBSTArray5[2], 75);
+
+    }
+
+    @Test
+    void generateBinaryArray() {
+        assertEquals(AlgorithmsDataStructures2.generateBinaryArray(abs1.length).length, 7);
+        assertEquals(AlgorithmsDataStructures2.generateBinaryArray(abs2.length).length, 7);
+        assertEquals(AlgorithmsDataStructures2.generateBinaryArray(abs3.length).length, 7);
+        assertEquals(AlgorithmsDataStructures2.generateBinaryArray(0).length, 0);
+        assertEquals(AlgorithmsDataStructures2.generateBinaryArray(1).length, 1);
+        assertEquals(AlgorithmsDataStructures2.generateBinaryArray(2).length, 3);
+        assertEquals(AlgorithmsDataStructures2.generateBinaryArray(3).length, 3);
+        assertEquals(AlgorithmsDataStructures2.generateBinaryArray(4).length, 7);
+        assertEquals(AlgorithmsDataStructures2.generateBinaryArray(15).length, 15);
+        assertEquals(AlgorithmsDataStructures2.generateBinaryArray(16).length, 31);
+
+    }
+}
