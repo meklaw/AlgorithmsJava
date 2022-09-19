67c1cacbd1ed0a1f23d6848d1036c338829f71d0
Никита Ульянов <nik.ule@yandex.ru>
2022-09-19 23:17:10 +0300

    add: BalancedBST

---
+++ src/main/java/BalancedBST.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+
+class BSTNode {
+    public int NodeKey; // ключ узла
+    public BSTNode Parent; // родитель или null для корня
+    public BSTNode LeftChild; // левый потомок
+    public BSTNode RightChild; // правый потомок	
+    public int Level; // глубина узла
+
+    public BSTNode(int key, BSTNode parent) {
+        NodeKey = key;
+        Parent = parent;
+        LeftChild = null;
+        RightChild = null;
+    }
+}
+
+class BalancedBST {
+    public BSTNode Root; // корень дерева
+
+    public BalancedBST() {
+        Root = null;
+    }
+
+    public void GenerateTree(int[] a) {
+        Arrays.sort(a);
+        int mid = a.length / 2;
+        Root = new BSTNode(a[mid], null);
+        Root.Level = 0;
+        if (0 <= mid - 1)
+            fromArrToBST(Root, a, 0, mid - 1);
+        if (mid + 1 < a.length)
+            fromArrToBST(Root, a, mid + 1, a.length - 1);
+    }
+
+    private void fromArrToBST(BSTNode parent, int[] a, int start, int end) {
+        int mid = (start + end) / 2;
+        BSTNode newNode = new BSTNode(a[mid], parent);
+        newNode.Level = parent.Level + 1;
+        if (newNode.NodeKey < parent.NodeKey)
+            parent.LeftChild = newNode;
+        else
+            parent.RightChild = newNode;
+
+        if (start == end)
+            return;
+
+        if (0 <= mid - 1)
+            fromArrToBST(newNode, a, start, mid - 1);
+        if (mid + 1 < a.length)
+            fromArrToBST(newNode, a, mid + 1, end);
+    }
+
+    public boolean IsBalanced(BSTNode root_node) {
+        if (root_node.LeftChild == null && root_node.RightChild == null)
+            return true;
+
+        int rightLevel = maxLevel(root_node.RightChild);
+        int leftLevel = maxLevel(root_node.LeftChild);
+
+        if (Math.abs(leftLevel - rightLevel) - root_node.Level > 1)
+            return false;
+
+        return true; // сбалансировано ли дерево с корнем root_node
+    }
+
+    private int maxLevel(BSTNode root_node) {
+        if (root_node == null)
+            return 0;
+        return Math.max(root_node.Level, Math.max(maxLevel(root_node.LeftChild), maxLevel(root_node.RightChild)));
+    }
+}  
---
+++ src/test/java/BalancedBSTTest.java
@@ -0,0 +1,120 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BalancedBSTTest {
+
+    BalancedBST bst;
+    int[] arr1;
+    int[] arr2;
+    int[] arr3;
+    int[] arr4;
+    int[] arr5;
+
+    @BeforeEach
+    void setUp() {
+        bst = new BalancedBST();
+        arr1 = new int[]{50};
+        arr2 = new int[]{50, 75};
+        arr3 = new int[]{25, 50, 75};
+        arr4 = new int[]{20, 25, 50, 75};
+        arr5 = new int[]{20, 25, 37, 50, 62, 75, 84};
+    }
+
+    @Test
+    void generateTree() {
+        bst.GenerateTree(arr1);
+        assertEquals(bst.Root.NodeKey, 50);
+        assertEquals(bst.Root.Parent, null);
+        assertEquals(bst.Root.LeftChild, null);
+        assertEquals(bst.Root.RightChild, null);
+        assertEquals(bst.Root.Level, 0);
+
+        bst.GenerateTree(arr2);
+        assertEquals(bst.Root.NodeKey, 75);
+        assertEquals(bst.Root.LeftChild.NodeKey, 50);
+        assertEquals(bst.Root.LeftChild.Parent.NodeKey, 75);
+        assertEquals(bst.Root.RightChild, null);
+
+        bst.GenerateTree(arr3);
+        assertEquals(bst.Root.NodeKey, 50);
+        assertEquals(bst.Root.LeftChild.NodeKey, 25);
+        assertEquals(bst.Root.LeftChild.Parent.NodeKey, 50);
+        assertEquals(bst.Root.LeftChild.LeftChild, null);
+        assertEquals(bst.Root.LeftChild.RightChild, null);
+        assertEquals(bst.Root.RightChild.NodeKey, 75);
+        assertEquals(bst.Root.RightChild.Parent.NodeKey, 50);
+        assertEquals(bst.Root.RightChild.LeftChild, null);
+        assertEquals(bst.Root.RightChild.RightChild, null);
+
+        bst.GenerateTree(arr4);
+        assertEquals(bst.Root.NodeKey, 50);
+        assertEquals(bst.Root.LeftChild.NodeKey, 20);
+        assertEquals(bst.Root.LeftChild.Parent.NodeKey, 50);
+        assertEquals(bst.Root.LeftChild.LeftChild, null);
+        assertEquals(bst.Root.LeftChild.RightChild.NodeKey, 25);
+        assertEquals(bst.Root.LeftChild.RightChild.Parent.NodeKey, 20);
+        assertEquals(bst.Root.RightChild.NodeKey, 75);
+        assertEquals(bst.Root.RightChild.Parent.NodeKey, 50);
+        assertEquals(bst.Root.RightChild.LeftChild, null);
+        assertEquals(bst.Root.RightChild.RightChild, null);
+
+
+        bst.GenerateTree(arr5);
+        assertEquals(bst.Root.NodeKey, 50);
+        assertEquals(bst.Root.LeftChild.NodeKey, 25);
+        assertEquals(bst.Root.LeftChild.Level, 1);
+        assertEquals(bst.Root.RightChild.NodeKey, 75);
+        assertEquals(bst.Root.RightChild.Level, 1);
+        assertEquals(bst.Root.LeftChild.LeftChild.NodeKey, 20);
+        assertEquals(bst.Root.LeftChild.LeftChild.Level, 2);
+        assertEquals(bst.Root.LeftChild.RightChild.NodeKey, 37);
+        assertEquals(bst.Root.LeftChild.RightChild.Level, 2);
+        assertEquals(bst.Root.RightChild.LeftChild.NodeKey, 62);
+        assertEquals(bst.Root.RightChild.LeftChild.Level, 2);
+        assertEquals(bst.Root.RightChild.RightChild.NodeKey, 84);
+        assertEquals(bst.Root.RightChild.RightChild.Level, 2);
+
+        assertEquals(bst.Root.LeftChild.LeftChild.LeftChild, null);
+        assertEquals(bst.Root.LeftChild.LeftChild.RightChild, null);
+        assertEquals(bst.Root.LeftChild.RightChild.LeftChild, null);
+        assertEquals(bst.Root.LeftChild.RightChild.RightChild, null);
+        assertEquals(bst.Root.RightChild.LeftChild.LeftChild, null);
+        assertEquals(bst.Root.RightChild.LeftChild.RightChild, null);
+        assertEquals(bst.Root.RightChild.RightChild.LeftChild, null);
+        assertEquals(bst.Root.RightChild.RightChild.RightChild, null);
+
+
+        assertEquals(bst.Root.LeftChild.Parent.NodeKey, 50);
+        assertEquals(bst.Root.RightChild.Parent.NodeKey, 50);
+        assertEquals(bst.Root.LeftChild.LeftChild.Parent.NodeKey, 25);
+        assertEquals(bst.Root.LeftChild.RightChild.Parent.NodeKey, 25);
+        assertEquals(bst.Root.RightChild.LeftChild.Parent.NodeKey, 75);
+        assertEquals(bst.Root.RightChild.RightChild.Parent.NodeKey, 75);
+
+
+    }
+
+    @Test
+    void isBalanced() {
+        bst.GenerateTree(arr1);
+        assertTrue(bst.IsBalanced(bst.Root));
+        bst.GenerateTree(arr2);
+        assertTrue(bst.IsBalanced(bst.Root));
+        bst.GenerateTree(arr3);
+        assertTrue(bst.IsBalanced(bst.Root));
+        bst.GenerateTree(arr4);
+        assertTrue(bst.IsBalanced(bst.Root));
+        bst.GenerateTree(arr5);
+        assertTrue(bst.IsBalanced(bst.Root));
+
+        bst.GenerateTree(arr2);
+        bst.Root.LeftChild.LeftChild = new BSTNode(10, bst.Root.LeftChild);
+        bst.Root.LeftChild.LeftChild.Level = 2;
+        assertFalse(bst.IsBalanced(bst.Root));
+        bst.Root.LeftChild.LeftChild.LeftChild = new BSTNode(5, bst.Root.LeftChild.LeftChild);
+        bst.Root.LeftChild.LeftChild.LeftChild.Level = 3;
+        assertFalse(bst.IsBalanced(bst.Root.LeftChild));
+    }
+}
